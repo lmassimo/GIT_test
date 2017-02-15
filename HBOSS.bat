6f82b7b603965830c529d21347d463162e7dfb7e
lmassimo <l.massimo@tiscali.it>
2017-02-15 12:32:06 +0100

    prova1
    prova1

---
+++ HBOSS.bat
@@ -0,0 +1,2 @@
+set firefox="C:\Installed\Firefox\firefox.exe"
+%firefox% http://127.0.0.1:8080/
---
+++ HGUI.bat
@@ -0,0 +1,2 @@
+set firefox="C:\Installed\Firefox\firefox.exe"
+%firefox% http://127.0.0.1:8080/AMM-GUI/resources/security/login.jsp
